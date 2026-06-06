3b8b9855b11027065347ba093648f027870f9f66
Ricky Alturino <alturino001@gmail.com>
2026-06-06 19:22:30 +0700

    chore: fix user creation postgres in ./db_init/init.sh

--- db_init/init.sh
+++ db_init/init.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 set -e
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<EOSQL
-  -- pdf_extractor: processes attachments, needs read/write on attachments and announcements
-  CREATE USER ${POSTGRES_BLOODHOUND_PDF_EXTRACTOR_USER} WITH PASSWORD '${POSTGRES_BLOODHOUND_PDF_EXTRACTOR_PASSWORD}';
-  GRANT CONNECT ON DATABASE bloodhound TO bloodhound_pdf_extractor;
-  GRANT USAGE ON SCHEMA public TO bloodhound_pdf_extractor;
-  GRANT SELECT, INSERT, UPDATE ON ALL TABLES IN SCHEMA public TO bloodhound_pdf_extractor;
-  ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT, INSERT, UPDATE ON TABLES TO bloodhound_pdf_extractor;
+psql -v ON_ERROR_STOP=1 \
+  --username "$POSTGRES_USER" \
+  --dbname "$POSTGRES_DB" \
+  -v extractor_user="$POSTGRES_BLOODHOUND_PDF_EXTRACTOR_USER" \
+  -v extractor_password="$POSTGRES_BLOODHOUND_PDF_EXTRACTOR_PASSWORD" \
+  -v analyzer_user="$POSTGRES_BLOODHOUND_PDF_ANALYZER_USER" \
+  -v analyzer_password="$POSTGRES_BLOODHOUND_PDF_ANALYZER_PASSWORD" <<EOSQL
 
-  -- analyzer: read/write access for analysis workloads
-  CREATE USER ${POSTGRES_BLOODHOUND_ANALYZER_USER} WITH PASSWORD '${POSTGRES_BLOODHOUND_ANALYZER_PASSWORD}';
-  GRANT CONNECT ON DATABASE bloodhound TO bloodhound_analyzer;
-  GRANT USAGE ON SCHEMA public TO bloodhound_analyzer;
-  GRANT SELECT, INSERT, UPDATE ON ALL TABLES IN SCHEMA public TO bloodhound_analyzer;
-  ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT, INSERT, UPDATE ON TABLES TO bloodhound_analyzer;
+  -- pdf_extractor
+  CREATE USER :extractor_user WITH PASSWORD ':extractor_password';
+  GRANT CONNECT ON DATABASE bloodhound TO :extractor_user;
+  GRANT USAGE ON SCHEMA public TO :extractor_user;
+  GRANT SELECT, INSERT, UPDATE ON ALL TABLES IN SCHEMA public TO :extractor_user;
+  ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT, INSERT, UPDATE ON TABLES TO :extractor_user;
+
+  -- analyzer
+  CREATE USER :analyzer_user WITH PASSWORD ':analyzer_password';
+  GRANT CONNECT ON DATABASE bloodhound TO :analyzer_user;
+  GRANT USAGE ON SCHEMA public TO :analyzer_user;
+  GRANT SELECT, INSERT, UPDATE ON ALL TABLES IN SCHEMA public TO :analyzer_user;
+  ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT, INSERT, UPDATE ON TABLES TO :analyzer_user;
 EOSQL
