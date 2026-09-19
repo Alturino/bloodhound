704c050157a1dc5728a891d86e1af69e176161c7
Ricky Alturino <alturino001@gmail.com>
2026-09-20 06:00:27 +0700

    move bloodhound to fetcher submodule

--- internal/config/scheduler.go
+++
@@ -1,9 +0,0 @@
-package config
-
-import "time"
-
-type Scheduler struct {
-	Interval       time.Duration `mapstructure:"interval"        json:"interval"`
-	CronExpr       string        `mapstructure:"cron_expr"       json:"cron_expr"`       // e.g., "*/15 * * * *"
-	HistoricalCron string        `mapstructure:"historical_cron" json:"historical_cron"` // "0 18 * * *" (6pm daily)
-}
--- internal/config/stockbit.go
+++
@@ -1,6 +0,0 @@
-package config
-
-type Stockbit struct {
-	Token   string `mapstructure:"token"    json:"token"`
-	BaseURL string `mapstructure:"base_url" json:"base_url"`
-}
